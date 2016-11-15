1c33e15634adbf26f329ba629451516887c8b6bd
Vishnu.t <vishnut@itp-mac001.thinkpalm.lan>
2016-11-15 16:03:33 +0530

    Added new controllers

---
+++ HelloWorld/VTAlertControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  VTAlertControllerViewController.h
+//  HelloWorld
+//
+//  Created by Vishnu T Vijay on 15/11/16.
+//  Copyright © 2016 thinkpalm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VTAlertControllerViewController : UIViewController
+
+@end
--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -25,5 +25,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showAlert:(id)sender {
+}
 
 @end
